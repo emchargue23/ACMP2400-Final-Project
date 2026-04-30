184b8114d3b1fb1d82232fedae76dd935f1e529b
Elaina McHargue <165853109+emchargue23@users.noreply.github.com>
2026-04-30 14:43:51 -0500

    Create main.tf

---
+++ stage2/main.tf
@@ -0,0 +1,52 @@
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "DJANGO_SECRET_KEY_PROD" {}
+
+terraform {
+  required_providers {
+    azurem = {
+      source = "hashicorp/azurem"
+      version = "4.68.0"
+    }
+  }
+backend "azurem" {
+  resource_group_name = "rg-acmp-final"
+  storage_account_name = "acmp2400storageaccount"
+  container_name = "big-tf-state-acmp2400"
+  use_azuread_auth = true
+}
+}
+
+provider "azurem" {
+  features {}
+}
+
+resource "azurem_container_group" "emchargue-aci" {
+  name = "acremchargueacmp2400"
+  resource_group_name = "rg-emchargue"
+  ip_address_type = "Public"
+  dms_name_label = "acmp-emchargue-instance"
+  os_type = "macOS"
+
+  container {
+    name = "final"
+    image = "acemchargueacmp2400.azurecr.io/final:latest"
+    cpu = "0.5"
+    memory = "1.5"
+
+  ports  {
+    port = 8000
+    protocol = "TCP"
+    }
+
+  secure_environment_variables = {
+    DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY_PROD
+  }
+
+  image_registry_credential {
+    server = "acemchargueacmp2400.azurecr.io"
+    username = var ARM_CLIENT_ID
+    password = var ARM_CLIENT_SECRET
+
+  }
+}
