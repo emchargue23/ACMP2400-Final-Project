1a57be6af94aad4011798ee696cbe6409eff33ef
Elaina McHargue <165853109+emchargue23@users.noreply.github.com>
2026-04-30 15:42:58 -0500

    Update main.tf
    updated names

--- stage2/main.tf
+++ stage2/main.tf
@@ -4,33 +4,42 @@ variable "DJANGO_SECRET_KEY_PROD" {}
 
 terraform {
   required_providers {
-    azurem = {
-      source = "hashicorp/azurem"
+    azurers = {
+      source = "hashicorp/azurers"
       version = "4.68.0"
     }
   }
-backend "azurem" {
-  resource_group_name = "rg-acmp-final"
-  storage_account_name = "acmp2400storageaccount"
-  container_name = "big-tf-state-acmp2400"
-  use_azuread_auth = true
-}
-}
+  
+  backend "azurem" {
+    resource_group_name = "rg.acmp.final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    use_azuread_auth = true
+  }
 
-provider "azurem" {
-  features {}
-}
+  provider "azurem" {
+    features {}
+  }
+
+  resource "azurem_container_registry" "emchargue-acr" {
+    name = "acr_emchargueacmp2400"
+    resource_group_name = "rg-emchargue"
+    location = "Central US"
+    sku = "Basic"
+    admin_enabled = false
+  }
 
 resource "azurem_container_group" "emchargue-aci" {
-  name = "acremchargueacmp2400"
+  name = "acmp2400-emchargue-aci"
+  location = "Central US"
   resource_group_name = "rg-emchargue"
   ip_address_type = "Public"
   dms_name_label = "acmp-emchargue-instance"
-  os_type = "macOS"
+  os_type = "Linux"
 
   container {
     name = "final"
-    image = "acemchargueacmp2400.azurecr.io/final:latest"
+    image = "acr_emchargueacmp2400.azurecr.io/final:latest"
     cpu = "0.5"
     memory = "1.5"
 
